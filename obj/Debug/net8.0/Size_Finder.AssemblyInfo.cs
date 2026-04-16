beb9072542ecd6710ad0c46ddf06b13c2706498a
Arunyogeswaran <arun.impulse@gmail.com>
2026-04-16 19:22:16 +0530

    Fix net9.0 target framework

--- obj/Debug/net8.0/Size_Finder.AssemblyInfo.cs
+++ obj/Debug/net8.0/Size_Finder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Size_Finder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76be3b8440d95d2f83b5281be505b49254231f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Size_Finder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Size_Finder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
